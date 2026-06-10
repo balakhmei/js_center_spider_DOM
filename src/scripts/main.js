82417739804b575353e83acf088a4fbad902e9bd
Ihor Balakhmei <ihor@MacBook-Pro-Ihor.local>
2026-06-10 21:03:40 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const spiderDim = spider.getBoundingClientRect();
+
+const wall = document.querySelector('.wall');
+const wallDim = wall.getBoundingClientRect();
+
+const leftSpider = (wallDim.width - spiderDim.width) / 2 - 10;
+const topSpider = (wallDim.height - spiderDim.height) / 2 - 10;
+
+spider.style.left = leftSpider + 'px';
+spider.style.top = topSpider + 'px';
